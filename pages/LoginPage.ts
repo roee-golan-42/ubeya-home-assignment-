0ca51e614a08203d0eb089a91623d20f603cfdc5
roee golan <roee.golan42@gmail.com>
2025-06-19 15:58:29 +0300

    init

---
+++ pages/LoginPage.ts
@@ -0,0 +1,21 @@
+import { Page } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class LoginPage extends BasePage {
+    private readonly userNameField;
+    private readonly passwordField;
+    private readonly loginButton;
+
+    constructor(page: Page) {
+        super(page)
+        this.userNameField = this.page.getByTestId('username');
+        this.passwordField = this.page.getByTestId('password');
+        this.loginButton = this.page.getByTestId('login-button');
+    }
+
+    public async login(userName: string, password: string) {
+        await this.userNameField.fill(userName);
+        await this.passwordField.fill(password);
+        await this.loginButton.click();
+    }
+}
